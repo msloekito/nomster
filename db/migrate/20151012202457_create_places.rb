ba82721130884f1fc954c17593a4d03b0d7957ca
msloekito <msloekito@gmail.com>
2015-10-12 20:59:06 +0000

    added data to database

--- db/migrate/20151012202457_create_places.rb
+++ db/migrate/20151012202457_create_places.rb
@@ -2,10 +2,11 @@ class CreatePlaces < ActiveRecord::Migration
   def change
     create_table :places do |t|
       
-      t.string :namespace
+      t.string :name
       t.text :description
       t.string :address
-      t.timestamps null: false
+      
+      t.timestamps
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -17,11 +17,11 @@ ActiveRecord::Schema.define(version: 20151012202457) do
   enable_extension "plpgsql"
 
   create_table "places", force: :cascade do |t|
-    t.string   "namespace"
+    t.string   "name"
     t.text     "description"
     t.string   "address"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end

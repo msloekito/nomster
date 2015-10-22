5d6b1279758069b04840bfd930fa4b54fb6e61bb
msloekito <msloekito@gmail.com>
2015-10-22 16:41:12 +0000

    allow editing place only by user who created

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-    before_filter :authenticate_user!, only: [:new, :create]
+    before_filter :authenticate_user!, only: [:new, :create, :edit, :update]
     
     def index
         @places = Place.order("created_at ASC").paginate(:page => params[:page], :per_page => 5)
@@ -20,18 +20,33 @@ class PlacesController < ApplicationController
     
     def edit
         @place = Place.find(params[:id])
+        
+        if @place.user != current_user
+            return render :text => 'Not Allowed', :status => :forbidden
+        end
     end
     
     def update
         @place = Place.find(params[:id])
+        
+        if @place.user != current_user
+            return render :text => 'Not Allowed!', :status => :forbidden
+        end    
         @place.update_attributes(place_params)
         redirect_to root_path
     end
     
     def destroy
         @place = Place.find(params[:id])
+        
+        if @place.user != current_user
+            return render :text => 'Not Allowed!', :status => :forbidden
+        end    
         @place.destroy
         redirect_to root_path
+        
+        
+        
     end
         
     private
